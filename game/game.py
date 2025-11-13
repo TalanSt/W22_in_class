4253811a58dc9ee2e8876851327dc51214b23e74
Talan122 <yellowramairta@gmail.com>
2025-11-12 21:18:35 -0500

    Handle random

--- game/game.py
+++ game/game.py
@@ -1,1 +1,9 @@
+from random import randint
+
 print("Welcome to rock paper scissors!")
+
+rand = randint(1, 3)
+
+if(rand == 1): print("Computer chose rock")
+if(rand == 2): print("Computer chose paper")
+if(rand == 3): print("Computer chose scissors")
